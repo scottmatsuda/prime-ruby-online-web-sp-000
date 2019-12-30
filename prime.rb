ef50b7e111eb0a937649157193b284bf0f53ddcd
Scott Matsuda <smatsuda2014@gmail.com>
2019-12-30 02:58:25 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -4,10 +4,6 @@ def prime?(integer)
     return false
   end
 
-  # if integer == 2
-  #   return true
-  # end
-  
   int_array = (2..integer - 1).to_a
   int_array.none? { |num| integer % num == 0 }
 
