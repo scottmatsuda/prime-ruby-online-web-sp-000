15d3f0dd8aebb9b1c24b1f6c951c5f4a5d0c2237
Scott Matsuda <smatsuda2014@gmail.com>
2019-12-30 02:58:29 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,10 +1,10 @@
 def prime?(integer)
+  
   if integer <= 1
     return false
   end
-  if integer == 2 || integer == 3
-    return true
-  end
+
   int_array = (2..integer - 1).to_a
   int_array.none? { |num| integer % num == 0 }
+
 end
